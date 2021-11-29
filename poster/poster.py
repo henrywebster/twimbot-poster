0cd52cfb308eacd9c17fd5af97f11a1ac4b1612a
Henry Webster <hwebs@hwebs.info>
2021-11-28 20:22:45 -0500

    use lazy logging

--- poster/poster.py
+++ poster/poster.py
@@ -33,7 +33,7 @@ class DynamoDBJournal(Journal):
     """
 
     def __init__(self, dynamodb_table, index):
-        logger.info("DynamoDB table (index) - {} ({})", dynamodb_table.name, index)
+        logger.info("DynamoDB table (index) - %s (%s)", dynamodb_table.name, index)
         self.dynamodb_table = dynamodb_table
         self.index = index
 
@@ -67,7 +67,7 @@ class S3ImageHandler(ImageHandler):  # pylint: disable=too-few-public-methods
     """
 
     def __init__(self, s3_bucket):
-        logger.info("S3 bucket - {}", s3_bucket.name)
+        logger.info("S3 bucket - %s", s3_bucket.name)
         self.s3_bucket = s3_bucket
 
     def handle(self, filename, callback):
