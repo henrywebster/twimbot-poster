6695c833916bc2beb1ad0fb4ab3747371015f8a5
Henry Webster <hwebs@hwebs.info>
2022-02-06 13:57:06 -0500

    remove requests library

--- twimbot_poster/app.py
+++ twimbot_poster/app.py
@@ -2,21 +2,11 @@ import os
 import json
 import boto3
 
-import requests
-
 
 def lambda_handler(event, context):
     """
     Sample pure Lambda function
     """
-
-    try:
-        ip = requests.get("http://checkip.amazonaws.com/")
-    except requests.RequestException as e:
-        # Send some context about this error to Lambda Logs
-        print(e)
-
-        raise e
 
     dynamodb_table = boto3.resource(
         "dynamodb", region_name=os.getenv("AWS_REGION")
