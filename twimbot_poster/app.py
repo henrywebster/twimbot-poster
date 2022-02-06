c7ddde13db4b8804b27927f28d41253dfa8e32ee
Henry Webster <hwebs@hwebs.info>
2022-02-06 13:56:18 -0500

    test using lambda

--- twimbot_poster/app.py
+++ twimbot_poster/app.py
@@ -1,4 +1,6 @@
+import os
 import json
+import boto3
 
 import requests
 
@@ -16,9 +18,16 @@ def lambda_handler(event, context):
 
         raise e
 
+    dynamodb_table = boto3.resource(
+        "dynamodb", region_name=os.getenv("AWS_REGION")
+    ).Table(os.getenv("DYNAMODB_TABLE"))
+
     return {
         "statusCode": 200,
         "body": json.dumps(
-            {"message": "hello world", "location": ip.text.replace("\n", "")}
+            {
+                "message": "hello world",
+                "data": dynamodb_table.scan()["Items"][0]["title"],
+            }
         ),
     }
